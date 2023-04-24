9dbcf7eebe9ab780980276dedae59e43feda9a93
LiMo Kiprono <94519855+limo39@users.noreply.github.com>
2023-04-25 01:13:25 +0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
-from .models import Booking, Flight
-from .forms import BookingForm
+from .models import Booking, Flight, Airplane, Payment
+from .forms import BookingForm, AirplaneForm, PaymentForm
+from django.contrib import messages
+from django.conf import settings
+import stripe
 
-def book_flight(request, flight_id):
+def book_flight(request, flight_id=None):
+    if flight_id is None:
+        return redirect('select_flight')
+
     flight = Flight.objects.get(pk=flight_id)
     passenger_form = PassengerForm(request.POST or None)
 
@@ -26,3 +32,66 @@ def home(request):
 
 def flight(request):
     return render(request, 'flight.html')
+
+def add_airplane(request):
+    if request.method == 'POST':
+        form = AirplaneForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('airplanes')
+    else:
+        form = AirplaneForm()
+
+    return render(request, 'add_airplane.html', {'form': form})
+
+def select_flight(request):
+    flights = Flight.objects.all()
+    return render(request, 'select_flight.html', {'flights': flights})
+
+def flight_detail(request, flight_id):
+    flight = get_object_or_404(Flight, pk=flight_id)
+    context = {'flight': flight}
+    return render(request, 'flight_detail.html', context)
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+def payment(request):
+    if request.method == 'POST':
+        form = PaymentForm(request.POST)
+        if form.is_valid():
+            # Retrieve the amount and currency from the form data
+            amount = int(form.cleaned_data['amount'])
+            currency = form.cleaned_data['currency']
+            # Create a new payment object
+            payment = Payment(amount=amount, currency=currency, user=request.user)
+            payment.save()
+            try:
+                # Use Stripe API to create a new charge
+                charge = stripe.Charge.create(
+                    amount=amount * 100,
+                    currency=currency,
+                    description='Flight booking payment',
+                    source=form.cleaned_data['stripe_token']
+                )
+                # Update the payment status to completed
+                payment.status = Payment.COMPLETED
+                payment.save()
+                messages.success(request, 'Payment completed successfully.')
+                return redirect('booking_confirmation')
+            except stripe.error.CardError as e:
+                # Handle card errors
+                payment.status = Payment.FAILED
+                payment.save()
+                messages.error(request, str(e))
+            except stripe.error.StripeError:
+                # Handle other Stripe errors
+                payment.status = Payment.FAILED
+                payment.save()
+                messages.error(request, 'An error occurred while processing your payment.')
+        else:
+            form = PaymentForm()
+        context = {
+            'form': form,
+        }
+        return render(request, 'payment.html', context)
+
