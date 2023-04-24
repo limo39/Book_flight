6cc4932a7b49f8e9200d0d46cd640566aa3c9bdb
LiMo Kiprono <94519855+limo39@users.noreply.github.com>
2023-04-25 01:14:25 +0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from .views import add_airplane
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('flight/', views.flight, name='flight'),
     path('book-flight/', views.book_flight, name='book_flight'),
+    path('add_airplane/', add_airplane, name='add_airplane'),
+    path('select-flight/', views.select_flight, name='select_flight'),
+    path('flight_detail/', views.flight_detail, name='flight_detail'),
 ]
- #path('book-flight/<int:flight_id>/', views.book_flight, name='book_flight'),
